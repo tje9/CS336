720ead049f55c98c998780250ec4153a13020c9e
Trevor Edewaard <tje9@babbage.cs.calvin.edu>
2016-11-16 13:50:42 -0500

    add lab11 back

---
+++ app/scripts/comment.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+import $ from 'jquery';
+
+import '../css/base.css';
+
+module.exports = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
